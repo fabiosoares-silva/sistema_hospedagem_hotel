d712cfa38b76bf22471dbc6685c684b65c17b0a1
fabiosoares-silva <fabiosores.soares@gmail.com>
2024-11-19 14:54:18 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Sistema_de_Hospedagem.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+hospedes.Add(p1);
+hospedes.Add(p2);
+
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 5, valorDiaria: 30);
+
+Reserva reserva = new Reserva(diasReservados: 10);
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"valor diária: {reserva.CalcularValorDiaria()}");
+
+
+
